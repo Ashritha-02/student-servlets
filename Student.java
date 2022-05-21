42cafe2606c7b8199c64944461830a171f87c22b
Ashritha-02 <73234111+Ashritha-02@users.noreply.github.com>
2022-05-21 17:44:58 +0530

    Add files via upload

--- Student.java
+++ Student.java
@@ -7,6 +7,9 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 import java.io.PrintWriter;
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -21,40 +24,57 @@ public class Student extends HttpServlet {
 		PrintWriter out=response.getWriter();
 		
 		String Sid=request.getParameter("Sid");
-		
+		JSONArray json = new JSONArray();
 		try {
 
 			Class.forName("com.mysql.cj.jdbc.Driver");
 			Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/railway", "root", "ashritha");
 			PreparedStatement ps = con.prepareStatement ("select * from student where Sid=?");
 			            ps.setString (1, Sid);
-			            out.print ("<table width=50% border=1>");
-			            out.print ("<caption>Student Details:</caption>");
+			           // out.print ("<table width=50% border=1>");
+			            //out.print ("<caption>Student Details:</caption>");
 			            ResultSet rs = ps.executeQuery ();
 			            
-			            out.print ("</br></br>");
+			          //  out.print ("</br></br>");
 			            ResultSetMetaData rsmd = rs.getMetaData ();
-			            int total = rsmd.getColumnCount ();
-			            out.print ("<tr>");
-			            for (int i = 1; i <= total; i++)
-			         {
-			             out.print ("<th>" + rsmd.getColumnName (i) + "</th>");
-			         }
-			            out.print ("</tr>");
-			            /* Printing result */
-			            while (rs.next ())
-			         {
-			             out.print ("<tr><td>" + rs.getString (1) + "</td><td>" +  rs.getString (2) + " </td><td>" + rs.getString (3) + " </td><td>" + rs.getString(4) + "</td></tr>");
-			         }
-			            out.print ("</table>");
-			        }
-			        catch (Exception e2)
-			        {
-			            e2.printStackTrace ();
-			        }
-			        finally
-			        {
-			            out.close ();
-			        }
-			    }
-			}
+				          
+				           // ResultSetMetaData rsmd = rs.getMetaData();
+				            while(rs.next()) {
+				              int numColumns = rsmd.getColumnCount();
+				              JSONObject obj = new JSONObject();
+				              for (int i=1; i<=numColumns; i++) {
+				                String column_name = rsmd.getColumnName(i);
+				                obj.put(column_name, rs.getObject(column_name));
+				              }
+				              json.put(obj);
+				            }
+				           // return json;
+			            //int total = rsmd.getColumnCount ();
+			            String jsonString=json.toString();
+			            out.print (jsonString);
+			            }
+		catch(Exception e) {
+			            	e.printStackTrace();           }
+		}}
+			           //for (int i = 1; i <= total; i++)
+//			         {
+//			             out.print ("<th>" + rsmd.getColumnName (i) + "</th>");
+//			         }
+//			            out.print ("</tr>");
+//			            /* Printing result */
+//			            while (rs.next ())
+//			         {
+//			             out.print ("<tr><td>" + rs.getString (1) + "</td><td>" +  rs.getString (2) + " </td><td>" + rs.getString (3) + " </td><td>" + rs.getString(4) + "</td></tr>");
+//			         }
+//			            out.print ("</table>");
+//			        }
+//			        catch (Exception e2)
+//			        {
+//			            e2.printStackTrace ();
+//			        }
+//			        finally
+//			        {
+//			            out.close ();
+//			        }
+//			    }
+//			}
